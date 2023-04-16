b0f80305b1e351d03ee9b17a471cf62a333fa195
YohanDavion <yohandavion31@gmail.com>
2023-04-16 22:07:19 +0200

    update home component

--- src/main/webapp/app/home/home.component.ts
+++ src/main/webapp/app/home/home.component.ts
@@ -6,23 +6,35 @@ import { takeUntil } from 'rxjs/operators';
 import { AccountService } from 'app/core/auth/account.service';
 import { Account } from 'app/core/auth/account.model';
 
+import { SharedService } from '../services/shared.service';
+import { MessageService, PrimeNGConfig } from 'primeng/api';
+
 @Component({
   selector: 'jhi-home',
   templateUrl: './home.component.html',
   styleUrls: ['./home.component.scss'],
+  providers: [MessageService],
 })
 export class HomeComponent implements OnInit, OnDestroy {
   account: Account | null = null;
 
   private readonly destroy$ = new Subject<void>();
 
-  constructor(private accountService: AccountService, private router: Router) {}
+  constructor(
+    private accountService: AccountService,
+    private router: Router,
+    private sharedService: SharedService,
+    private messageService: MessageService,
+    private primeNGConfig: PrimeNGConfig
+  ) {}
 
   ngOnInit(): void {
     this.accountService
       .getAuthenticationState()
       .pipe(takeUntil(this.destroy$))
       .subscribe(account => (this.account = account));
+
+    this.primeNGConfig.ripple = true;
   }
 
   login(): void {
@@ -32,5 +44,16 @@ export class HomeComponent implements OnInit, OnDestroy {
   ngOnDestroy(): void {
     this.destroy$.next();
     this.destroy$.complete();
+  }
+
+  simulateConnection(): void {
+    this.sharedService.isConnected = !this.sharedService.isConnected;
+  }
+  getConnectionStatus(): boolean {
+    return this.sharedService.isConnected;
+  }
+
+  show() {
+    this.messageService.add({ key: 'bc', severity: 'success', summary: 'Success', detail: 'Message Content', life: 300000 });
   }
 }
--- src/main/webapp/app/home/home.module.ts
+++ src/main/webapp/app/home/home.module.ts
@@ -4,9 +4,12 @@ import { RouterModule } from '@angular/router';
 import { SharedModule } from 'app/shared/shared.module';
 import { HOME_ROUTE } from './home.route';
 import { HomeComponent } from './home.component';
+import { ToastModule } from 'primeng/toast';
+import { RippleModule } from 'primeng/ripple';
 
 @NgModule({
-  imports: [SharedModule, RouterModule.forChild([HOME_ROUTE])],
+  imports: [SharedModule, ToastModule, RippleModule, RouterModule.forChild([HOME_ROUTE])],
   declarations: [HomeComponent],
+  bootstrap: [HomeComponent],
 })
 export class HomeModule {}
