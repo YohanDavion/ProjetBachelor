f4c4750330760eef7f06846087c40be55d9177f4
YohanDavion <yohandavion31@gmail.com>
2023-05-29 21:37:03 +0200

    delete unused files

--- src/main/webapp/app/account/moncompte/moncompte.component.ts
+++
@@ -1,8 +0,0 @@
-import { Component } from '@angular/core';
-
-@Component({
-  selector: 'jhi-moncompte',
-  templateUrl: './moncompte.component.html',
-  styleUrls: ['./moncompte.component.scss'],
-})
-export class MoncompteComponent {}
