0652e4a26cda53d6af53b659e61c284e9d166207
Baptiste <baptiste.puibusque@limayrac.fr>
2023-07-12 14:13:25 +0200

    Suppression fichiers de trop

--- views/home.php
+++ views/home.php
@@ -28,11 +28,11 @@ if ($sliderRequete->rowCount() > 0) {
     </div>
     <button class="carousel-control-prev" type="button" data-bs-target="#carouselExampleControls" data-bs-slide="prev">
         <span class="carousel-control-prev-icon" aria-hidden="true"></span>
-        <span class="visually-hidden">Previous</span>
+        <span class="visually-hidden">Précédent</span>
     </button>
     <button class="carousel-control-next" type="button" data-bs-target="#carouselExampleControls" data-bs-slide="next">
         <span class="carousel-control-next-icon" aria-hidden="true"></span>
-        <span class="visually-hidden">Next</span>
+        <span class="visually-hidden">Suivant</span>
     </button>
 </div>
 
